b43528a3b70f93299579fd975cfd27d73dd9c8db
aCommonFool <robertcasson28@gmail.com>
2017-02-22 20:39:43 +0000

    Added service charge calculation
    The method Calculate will now apply a service charge appropriate to the
    items on the bill.

--- Cafe X Tip Calculator/BillCalculator.cs
+++ Cafe X Tip Calculator/BillCalculator.cs
@@ -8,45 +8,76 @@ namespace Cafe_X_Tip_Calculator
 {
     public class BillCalculator
     {
-        public const string COLA = "Cola";
-        public const float COLA_PRICE = 0.5F;
+        const string COLA = "Cola";
+        const double COLA_PRICE = 0.5;
 
-        public const string COFFEE = "Coffee";
-        public const float COFFEE_PRICE = 1.0F;
+        const string COFFEE = "Coffee";
+        const double COFFEE_PRICE = 1.0;
 
-        public const string CHEESE = "Cheese Sandwich";
-        public const float CHEESE_PRICE = 2.0F;
+        const string CHEESE = "Cheese Sandwich";
+        const double CHEESE_PRICE = 2.0;
 
-        public const string STEAK = "Steak Sandwich";
-        public const float STEAK_PRICE = 4.5F;
+        const string STEAK = "Steak Sandwich";
+        const double STEAK_PRICE = 4.5;
 
-        public float Calculate(List<String> a_Bill)
+        const int HOT_FOOD_CHARGE = 20;
+        const int FOOD_CHARGE = 10;
+
+        bool m_HasFood = false;
+        bool m_HasHotFood = false;
+
+        // Calculates Bill
+        public double Calculate(List<String> a_Bill)
         {
-            float l_Total = 0.0F;
-
+            double l_Total = 0.0;
+            
             foreach (string l_Item in a_Bill)
             {
                 switch(l_Item)
                 {
                     case COLA:
                         l_Total += COLA_PRICE;
-                        break;
+                        continue;
 
                     case COFFEE:
                         l_Total += COFFEE_PRICE;
-                        break;
+                        continue;
 
                     case CHEESE:
                         l_Total += CHEESE_PRICE;
-                        break;
+                        m_HasFood = true;
+                        continue;
 
                     case STEAK:
                         l_Total += STEAK_PRICE;
-                        break;
+                        m_HasHotFood = true;
+                        continue;
                 }
             }
 
-            return l_Total;
+            return AddServiceCharge(l_Total);
+        }
+
+        // Adds appropriate service charge
+        private double AddServiceCharge(double a_Total)
+        {
+            if(m_HasHotFood)
+            {
+                AddPecentage(ref a_Total, HOT_FOOD_CHARGE);
+            } 
+            else if (m_HasFood)
+            {
+                AddPecentage(ref a_Total, FOOD_CHARGE);
+            }
+
+            //Round to two decimal places
+            return Math.Round(a_Total, 2);
+        }
+
+        // Increases a_Value by % a_Percent
+        private void AddPecentage(ref double a_Value, int a_Percent)
+        {
+            a_Value += ( (a_Value / 100) * a_Percent );
         }
     }
 }
