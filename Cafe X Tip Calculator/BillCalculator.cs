30c6ba65b43e9b74b914138b80e771af942cbccb
aCommonFool <robertcasson28@gmail.com>
2017-02-22 19:45:23 +0000

    Initial commit
    Added Unit tests to develop against for stage one of the exercise.

---
+++ Cafe X Tip Calculator/BillCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_X_Tip_Calculator
+{
+    public class BillCalculator
+    {
+        public float Calculate(List<String> a_Bill)
+        {
+            return 0.0F;
+        }
+    }
+}
---
+++ Cafe X Tip CalculatorTests/BillCalculatorTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cafe_X_Tip_Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe_X_Tip_Calculator.Tests
+{
+    [TestClass()]
+    public class BillCalculatorTests
+    {
+
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            BillCalculator m_BillCalc = new BillCalculator();
+            List<String> l_TestData = new List<string>();
+
+            // Ensure empty list works
+            Assert.AreEqual(m_BillCalc.Calculate(l_TestData), 0.0);
+
+            l_TestData.Add("Cola");
+            l_TestData.Add("Coffee");
+            l_TestData.Add("Cheese Sandwich");
+            l_TestData.Add("Steak Sandwich");
+
+            // Test each element
+            Assert.AreEqual(m_BillCalc.Calculate(l_TestData), 3.5);
+
+            l_TestData.Add("UNKnOWN");
+
+            // Test Unknown element
+            Assert.AreEqual(m_BillCalc.Calculate(l_TestData), 3.5);
+        }
+    }
+}
